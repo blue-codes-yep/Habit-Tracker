301ccc28aa4980aa95b52f51464495857ec6dfc6
Blue Lindsey <linzblue5@gmail.com>
2020-05-18 19:20:38 -0500

    Set up inital graphql api

--- pages/api/graphql.js
+++ pages/api/graphql.js
@@ -1,6 +1,25 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import { ApolloServer, gql } from 'apollo-server-micro'
 
-export default (req, res) => {
-  res.statusCode = 200
-  res.json({ name: 'John Doe' })
+const typeDefs = gql`
+  type Query {
+    sayHello: String
 }
+`
+
+const resolvers = {
+  Query: {
+    sayHello: () => {
+      return 'Hello World!'
+    }
+  }
+}
+
+const apolloServer = new ApolloServer({ typeDefs, resolvers })
+
+export const config = {
+  api: {
+    bodyParser: false
+  }
+}
+
+export default apolloServer.createHandler({ path: "/api/graphql" })
