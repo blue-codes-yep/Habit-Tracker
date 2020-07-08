76c20f1f72179a80de064234737f9c6345e1d946
Blue Lindsey <linzblue5@gmail.com>
2020-07-08 17:16:56 -0500

    Added dates to each Habit button.

--- components/SingleHabit.jsx
+++ components/SingleHabit.jsx
@@ -1,18 +1,26 @@
 import HabitButton from './HabitButton'
 
 const SingleHabit = ({ habit }) => {
+    const dates = getWeek()
     return (
         <article>
             <h3>{habit}</h3>
             <div>
-                <HabitButton />
-                <HabitButton />
-                <HabitButton />
-                <HabitButton />
-                <HabitButton />
+                {dates.map(date =>
+                    (<HabitButton key={date.getMilliseconds()} date={date} />
+                    ))}
             </div>
         </article>
     )
+}
+
+const getWeek = () => {
+    const dates = '43210'.split('').map(day => {
+        const tempDate = new Date();
+        tempDate.setDate(tempDate.getDate() - day)
+        return tempDate;
+    })
+    return dates;
 }
 
 export default SingleHabit;
